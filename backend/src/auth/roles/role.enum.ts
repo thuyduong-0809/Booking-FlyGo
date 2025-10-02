638c5c336ff5fba8d5ac36390fee77aa55a1f79a
thuyduong-0809 <puppine0809@gmail.com>
2025-10-03 05:08:55 +0700

    create and use RolesGuard,roles decorator,role enum

---
+++ backend/src/auth/roles/role.enum.ts
@@ -0,0 +1,8 @@
+export enum Role {
+  Passenger = 'Passenger',
+  BookingAgent ='BookingAgent',
+  FlightManager = 'FlightManager',
+  CheckInStaff = 'CheckInStaff',
+  SystemAdmin = 'SystemAdmin',
+  AirlinePartner = 'AirlinePartner'
+}
